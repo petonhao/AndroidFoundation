26c99df3f1b67351709ceab7b26dd438c6523ff4
petonhao <petonhao123@gmail.com>
2016-11-26 17:55:09 +0800

    用户界面初始化

---
+++ userinterface/src/main/java/com/foundation/petonhao/userinterface/MainActivity.java
@@ -0,0 +1,15 @@
+package com.foundation.petonhao.userinterface;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "niaho", Toast.LENGTH_SHORT).show();
+    }
+}
